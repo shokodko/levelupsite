cf19416ecd7ba598b1eb5aaa716330da9ec0edb1
shokodko <extremeplayerpc0@gmail.com>
2020-01-18 14:42:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+		function test (a,b) {
+			return a+b
+		}
+		
+		let minus (a,b) => {
+			return a-b
+		}
+		
+		let plusMinus = (a,b,c) => {
+			return a + b - c 
+		}
+		
+		
+	consol.log(test(2,3))
+	consol.log(minus(5,3))
+	consol.log(plusMinus(5,5,7))
